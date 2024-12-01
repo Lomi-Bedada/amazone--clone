6bae655754391fe1bed8c4e92cc3a060367d2755
Lomi-Bedada <lomibedada110215@gmail.com>
2024-12-01 19:00:13 +0400

    function folderi removed

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  http: "127.0.0.1:5001/clone-6e083/us-central1/api",
+  // http: "127.0.0.1:5001/clone-6e083/us-central1/api",
 
   //deployed for firebase function
-  baseURL: "https://amazon-clone-back-u1rp.onrender.com",
+  // baseURL: "https://amazon-clone-back-u1rp.onrender.com",
   //deploy with server of github
   baseURL: "https:amazon-api-deploy-0b73.onrender.com/",
 });

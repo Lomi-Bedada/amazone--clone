2839189252d86095251ed08b339ab305c36a77c7
Lomi-Bedada <lomibedada110215@gmail.com>
2024-12-03 19:25:50 +0400

    as

--- src/pages/Land/Landing.jsx
+++ src/pages/Land/Landing.jsx
@@ -1,16 +1,16 @@
 import Carousel from "../../Components/Carousel/Carousel.jsx";
 import Category from "../../Components/Categorys/Category.jsx";
-import LayOut from "../../Layouts/LayOut.jsx";
+import Layout from "../../Layouts/LayOut.jsx";
 import Product from "../../Components/products/Product.jsx";
 import React from "react";
 
 const Landing = () => {
   return (
-    <LayOut>
+    <Layout>
       <Carousel />
       <Category />
       <Product />
-    </LayOut>
+    </Layout>
   );
 };
 
